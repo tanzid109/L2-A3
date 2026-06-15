f9ea04d72ad8d227b3d7190ffa014b3cacf3f29e
tanzid109 <tanzid.unkhalil109@gmail.com>
2026-06-15 15:27:11 +0600

    all query added

--- QUERY.sql
+++ QUERY.sql
@@ -1,5 +1,5 @@
 -- =========================================================================
--- 1. CREATE USERS TABLE
+--! 1. CREATE USERS TABLE
 -- =========================================================================
 CREATE TABLE IF NOT EXISTS Users(
     user_id SERIAL PRIMARY KEY,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS Users(
 );
 
 -- =========================================================================
--- 2. CREATE MATCHES TABLE
+--! 2. CREATE MATCHES TABLE
 -- =========================================================================
 
 CREATE TABLE IF NOT EXISTS Matches (
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Matches (
 );
 
 -- =========================================================================
--- 3. CREATE BOOKINGS TABLE
+--! 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE IF NOT EXISTS Bookings (
     booking_id SERIAL PRIMARY KEY,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Bookings (
 );
 
 -- =========================================================================
--- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
+--! DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
 INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
@@ -43,7 +43,7 @@ INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
 -- =========================================================================
--- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
+--! DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
 -- =========================================================================
 INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
 (101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
@@ -53,7 +53,7 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
 
 -- =========================================================================
--- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
+--! DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
 -- =========================================================================
 INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
 (501, 1, 101, 'A-12', 'Confirmed', 150.00),
@@ -61,3 +61,46 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+
+-- =========================================================================
+--! ALL QUERY----
+-- =========================================================================
+
+--* Query 1
+SELECT match_id, fixture, base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+--* Query 2
+SELECT user_id,full_name,email
+FROM users
+WHERE full_name LIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+--* Query 3
+SELECT booking_id,user_id,match_id,COALESCE(payment_status,'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
+
+--* Query 4
+SELECT b.booking_id,u.full_name,m.fixture,b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id=u.user_id
+INNER JOIN Matches m ON b.match_id=m.match_id;
+
+--* Query 5
+SELECT u.user_id,u.full_name,b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id=b.user_id
+
+--* Query 6
+SELECT booking_id,match_id,total_cost
+FROM Bookings
+WHERE total_cost>(SELECT AVG(total_cost) FROM Bookings)
+
+--* Query 7
+SELECT match_id,fixture,base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1
